f098f4b0d35694cb29a7646d47f3e43fedd55964
Kasparas Galdikas <kasparas.galdikas@rakuten.com>
2017-09-29 15:55:49 +0100

    Minesweeper progress

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -3,12 +3,23 @@ module.exports = class Minesweeper {
     this.rows = rows;
     this.columns = columns;
 
-    this.map = this.map();
+    this.map = this.createMap();
   }
 
-  map() {
+  createMap() {
+    const positionMine = (map) => {
+      // ...
+    };
+
     const row = new Array(this.columns).fill('.');
     const rows = new Array(this.rows).fill(row);
+
     return rows;
+  }
+
+  countMines() {
+    // Create an internal function that position a mine based on a ratio
+    // Then calculate how many mines we have on the map and return the number
+    return 1;
   }
 };
--- test/minesweeper_test.js
+++ test/minesweeper_test.js
@@ -3,18 +3,22 @@ const Minesweeper = require('../src/minesweeper');
 describe('minesweeper', () => {
   describe('map method', () => {
     it('given 2 rows and 4 columns, i see the map', () => {
-      //method creates array of arrays = map
-      //method that prints the map as a string (with . and *)
-      //
       const sweeper = new Minesweeper(2,4);
 
-      const expected =
-      [
-        ['.','.', '.', '.'],
-        ['.','.', '.', '.']
+      const expected = [
+        ['.', '.', '.', '.'],
+        ['.', '.', '.', '.']
       ];
 
-      expect(sweeper.map()).toEqual(expected);
+      expect(sweeper.createMap()).toEqual(expected);
+    });
+  });
+
+  describe('countMines()', () => {
+    it('returns 2 mines given a map of 2x4', () => {
+      const sweeper = new Minesweeper(2,4);
+
+      expect(sweeper.countMines()).toBe(2);
     });
   });
 });
